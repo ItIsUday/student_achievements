b4d87115bf3efa95b173818eed93250f074a8413
Uday A S <57130884+ItIsUday@users.noreply.github.com>
2021-12-08 23:41:03 +0530

    Add relevant models (#2)
    - Migrate from Django user model to custom user model (users.User)
    - Create home app
    - Add Organization and Achievement models in home app
    - Add Student and Counselor models that has one-to-one relationship with User model
    - Register models for admin

---
+++ home/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+class Organization(models.Model):
+    id = models.CharField(max_length=10, primary_key=True)
+    name = models.CharField(max_length=255)
+    type = models.CharField(max_length=25)
+
+    def __str__(self):
+        return f"{self.id} - {self.name}"
+
+
+class Achievement(models.Model):
+    id = models.CharField(max_length=10, primary_key=True)
+    title = models.CharField(max_length=255)
+    achievement_date = models.DateField()
+    academic_year = models.PositiveSmallIntegerField()
+    certificate = models.FileField()
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.id} - {self.title}"
--- users/models.py
+++ users/models.py
@@ -1,1 +1,30 @@
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from home.models import Achievement
+
+
+class User(AbstractUser):
+    is_student = models.BooleanField(default=False)
+    is_counselor = models.BooleanField(default=False)
+
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    usn = models.CharField(max_length=15, primary_key=True)
+    phone = models.CharField(max_length=10)
+    birth_date = models.DateField(blank=True)
+    counselor = models.ForeignKey("Counselor", on_delete=models.CASCADE)
+    achievements = models.ManyToManyField(Achievement, blank=True, related_name="holders")
+
+    def __str__(self):
+        return f"{self.usn} - {self.user.get_full_name()}"
+
+
+class Counselor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    id = models.CharField(max_length=15, primary_key=True)
+    phone = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"{self.id} - {self.user.get_full_name()}"
